721bc3bdb73cc38dcd244752fb34789da2471de7
iamlxb3 <iamlxb3@gmail.com>
2017-10-10 15:20:16 +0100

    222

---
+++ mlp_regressor.py
@@ -0,0 +1,44 @@
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# MLP regressor only for trading, currently for a share stock and forex
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# (c) 2017 PJS, University of Sheffield, iamlxb3@gmail.com
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# ==========================================================================================================
+# general import
+# ==========================================================================================================
+import numpy as np
+import pickle
+from sklearn.neural_network import MLPRegressor
+# ==========================================================================================================
+
+
+
+
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT I
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class MlpRegressor_P:
+
+    def __init__(self, hidden_layer_sizes, tol=1e-6, learning_rate_init=0.001):
+        self.mlp_regressor = MLPRegressor(hidden_layer_sizes=hidden_layer_sizes,
+                                          tol=tol, learning_rate_init=learning_rate_init,
+                                          max_iter=1000, random_state=1, verbose=True)
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # [C.1] Train and Dev
+    # ------------------------------------------------------------------------------------------------------------------
+    def regressor_train(self, feature_list, value_list):
+        self.mlp_regressor.fit(feature_list, value_list)
+
+
+    def regressor_dev(self, sample_now):
+        pred_value = np.array(self.mlp_regressor.predict(sample_now))[0]
+        return pred_value
